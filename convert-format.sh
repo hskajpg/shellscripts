62c48ab16ad33585429986779f5f9641d635f1a0
Pedro <mdsajpg@gmail.com>
2019-04-29 21:45:23 -0300

    A script for converting files format

---
+++ convert-format.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cd ~/Desktop/scripts #Change to the desired path
+
+convert_format(){
+
+echo "Insert the name of the directory where files are supposed to be located after conversion: "
+
+read dir_name
+
+
+if [ ! -d $dir_name ]; then
+	mkdir $dir_name
+fi
+
+echo "Insert the format of files to be converted: Ex:'pdf'"
+
+read initial_format
+
+echo "Insert the format to which the files will be converted to: Ex:'txt'"
+
+read final_format
+
+for file in *.$initial_format
+do
+	new_file=$(ls $file | awk -F. '{print $1}')
+	convert $new_file.$initial_format $dir_name/$new_file.$final_format
+done
+
+# The next line excludes all files in the initial format. Uncomment if you wish to do so otherwise keep it commented.
+# rm *.$initial_format
+
+}
+
+convert_format
+
+
